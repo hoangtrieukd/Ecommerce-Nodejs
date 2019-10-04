bf5e5c5431855ac4619ea0d78678c613217ff1fc
Trieu Dang <trieu.dang@saigontechnology.com>
2019-10-04 14:09:26 +0700

    match atlas mongo db

--- routes/index.js
+++ routes/index.js
@@ -1,58 +1,57 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 const ensureAuthenticated = require('../modules/ensureAuthenticated')
 const Product = require('../models/Product')
 const Variant = require('../models/Variant')
 const Department = require('../models/Department')
 const Category = require('../models/Category')
 const TypedError = require('../modules/ErrorHandler')
-const Cart = require('../models/Cart');
+const Cart = require('../models/Cart')
 const CartClass = require('../modules/Cart')
 const paypal_config = require('../configs/paypal-config')
 const paypal = require('paypal-rest-sdk')
 
-
 //GET /products
-router.get('/products', function (req, res, next) {
+router.get('/products', function(req, res, next) {
   const { query, order } = categorizeQueryString(req.query)
-  Product.getAllProducts(query, order, function (e, products) {
+  Product.getAllProducts(query, order, function(e, products) {
     if (e) {
-      e.status = 406; return next(e);
+      e.status = 406
+      return next(e)
     }
     if (products.length < 1) {
-      return res.status(404).json({ message: "products not found" })
+      return res.status(404).json({ message: 'products not found' })
     }
     res.json({ products: products })
   })
-});
+})
 
 //GET /products/:id
-router.get('/products/:id', function (req, res, next) {
-  let productId = req.params.id;
-  Product.getProductByID(productId, function (e, item) {
+router.get('/products/:id', function(req, res, next) {
+  let productId = req.params.id
+  Product.getProductByID(productId, function(e, item) {
     if (e) {
-      e.status = 404; return next(e);
-    }
-    else {
+      e.status = 404
+      return next(e)
+    } else {
       res.json({ product: item })
     }
-  });
-});
+  })
+})
 
 //GET /variants
-router.get('/variants', function (req, res, next) {
+router.get('/variants', function(req, res, next) {
   let { productId } = req.query
   if (productId) {
-    Variant.getVariantProductByID(productId, function (err, variants) {
+    Variant.getVariantProductByID(productId, function(err, variants) {
       if (err) return next(err)
       return res.json({ variants })
     })
   } else {
-    Variant.getAllVariants(function (e, variants) {
+    Variant.getAllVariants(function(e, variants) {
       if (e) {
         if (err) return next(err)
-      }
-      else {
+      } else {
         return res.json({ variants })
       }
     })
@@ -60,10 +59,10 @@ router.get('/variants', function (req, res, next) {
 })
 
 //GET /variants/:id
-router.get('/variants/:id', ensureAuthenticated, function (req, res, next) {
+router.get('/variants/:id', ensureAuthenticated, function(req, res, next) {
   let id = req.params.id
   if (id) {
-    Variant.getVariantByID(id, function (err, variants) {
+    Variant.getVariantByID(id, function(err, variants) {
       if (err) return next(err)
       res.json({ variants })
     })
@@ -71,49 +70,51 @@ router.get('/variants/:id', ensureAuthenticated, function (req, res, next) {
 })
 
 //GET /departments
-router.get('/departments', function (req, res, next) {
-  Department.getAllDepartments(function (err, d) {
+router.get('/departments', function(req, res, next) {
+  Department.getAllDepartments(function(err, d) {
     if (err) return next(err)
     res.status(200).json({ departments: d })
   })
 })
 
 //GET /categories
-router.get('/categories', function (req, res, next) {
-  Category.getAllCategories(function (err, c) {
+router.get('/categories', function(req, res, next) {
+  Category.getAllCategories(function(err, c) {
     if (err) return next(err)
     res.json({ categories: c })
   })
 })
 
 //GET /search?
-router.get('/search', function (req, res, next) {
+router.get('/search', function(req, res, next) {
   const { query, order } = categorizeQueryString(req.query)
   query['department'] = query['query']
   delete query['query']
-  Product.getProductByDepartment(query, order, function (err, p) {
+  Product.getProductByDepartment(query, order, function(err, p) {
     if (err) return next(err)
     if (p.length > 0) {
       return res.json({ products: p })
     } else {
       query['category'] = query['department']
       delete query['department']
-      Product.getProductByCategory(query, order, function (err, p) {
+      Product.getProductByCategory(query, order, function(err, p) {
         if (err) return next(err)
         if (p.length > 0) {
           return res.json({ products: p })
         } else {
           query['title'] = query['category']
           delete query['category']
-          Product.getProductByTitle(query, order, function (err, p) {
+          Product.getProductByTitle(query, order, function(err, p) {
             if (err) return next(err)
             if (p.length > 0) {
               return res.json({ products: p })
             } else {
               query['id'] = query['title']
               delete query['title']
-              Product.getProductByID(query.id, function (err, p) {
-                let error = new TypedError('search', 404, 'not_found', { message: "no product exist" })
+              Product.getProductByID(query.id, function(err, p) {
+                let error = new TypedError('search', 404, 'not_found', {
+                  message: 'no product exist'
+                })
                 if (err) {
                   return next(error)
                 }
@@ -132,20 +133,20 @@ router.get('/search', function (req, res, next) {
 })
 
 // GET filter
-router.get('/filter', function (req, res, next) {
+router.get('/filter', function(req, res, next) {
   let result = {}
   let query = req.query.query
-  Product.filterProductByDepartment(query, function (err, p) {
+  Product.filterProductByDepartment(query, function(err, p) {
     if (err) return next(err)
     if (p.length > 0) {
       result['department'] = generateFilterResultArray(p, 'department')
     }
-    Product.filterProductByCategory(query, function (err, p) {
+    Product.filterProductByCategory(query, function(err, p) {
       if (err) return next(err)
       if (p.length > 0) {
         result['category'] = generateFilterResultArray(p, 'category')
       }
-      Product.filterProductByTitle(query, function (err, p) {
+      Product.filterProductByTitle(query, function(err, p) {
         if (err) return next(err)
         if (p.length > 0) {
           result['title'] = generateFilterResultArray(p, 'title')
@@ -153,7 +154,9 @@ router.get('/filter', function (req, res, next) {
         if (Object.keys(result).length > 0) {
           return res.json({ filter: result })
         } else {
-          let error = new TypedError('search', 404, 'not_found', { message: "no product exist" })
+          let error = new TypedError('search', 404, 'not_found', {
+            message: 'no product exist'
+          })
           return next(error)
         }
       })
@@ -162,79 +165,83 @@ router.get('/filter', function (req, res, next) {
 })
 
 //GET /checkout
-router.get('/checkout/:cartId', ensureAuthenticated, function (req, res, next) {
+router.get('/checkout/:cartId', ensureAuthenticated, function(req, res, next) {
   const cartId = req.params.cartId
   const frontURL = 'https://zack-ecommerce-reactjs.herokuapp.com'
   // const frontURL = 'http://localhost:3000'
 
-  Cart.getCartById(cartId, function (err, c) {
+  Cart.getCartById(cartId, function(err, c) {
     if (err) return next(err)
     if (!c) {
-      let err = new TypedError('/checkout', 400, 'invalid_field', { message: 'cart not found' })
+      let err = new TypedError('/checkout', 400, 'invalid_field', {
+        message: 'cart not found'
+      })
       return next(err)
     }
     const items_arr = new CartClass(c).generateArray()
     const paypal_list = []
     for (const i of items_arr) {
       paypal_list.push({
-        "name": i.item.title,
-        "price": i.item.price,
-        "currency": "CAD",
-        "quantity": i.qty
+        name: i.item.title,
+        price: i.item.price,
+        currency: 'CAD',
+        quantity: i.qty
       })
     }
     const create_payment_json = {
-      "intent": "sale",
-      "payer": {
-        "payment_method": "paypal"
+      intent: 'sale',
+      payer: {
+        payment_method: 'paypal'
       },
-      "redirect_urls": {
-        "return_url": frontURL + '/success_page',
-        "cancel_url": frontURL + '/cancel_page'
+      redirect_urls: {
+        return_url: frontURL + '/success_page',
+        cancel_url: frontURL + '/cancel_page'
       },
-      "transactions": [{
-        "item_list": {
-          "items": paypal_list
-        },
-        "amount": {
-          "currency": "CAD",
-          "total": c.totalPrice
-        },
-        "description": "This is the payment description."
-      }]
+      transactions: [
+        {
+          item_list: {
+            items: paypal_list
+          },
+          amount: {
+            currency: 'CAD',
+            total: c.totalPrice
+          },
+          description: 'This is the payment description.'
+        }
+      ]
     }
-    paypal.configure(paypal_config);
-    paypal.payment.create(create_payment_json, function (error, payment) {
+    paypal.configure(paypal_config)
+    paypal.payment.create(create_payment_json, function(error, payment) {
       if (error) {
-        console.log(JSON.stringify(error));
+        console.log(JSON.stringify(error))
         return next(error)
       } else {
-        console.log(payment);
+        console.log(payment)
         for (const link of payment.links) {
           if (link.rel === 'approval_url') {
             res.json(link.href)
           }
         }
       }
-    });
+    })
   })
 })
 
 //GET /payment/success
-router.get('/payment/success', ensureAuthenticated, function (req, res, next) {
-  var paymentId = req.query.paymentId;
-  var payerId = { payer_id: req.query.PayerID };
-  paypal.payment.execute(paymentId, payerId, function (error, payment) {
+router.get('/payment/success', ensureAuthenticated, function(req, res, next) {
+  var paymentId = req.query.paymentId
+  var payerId = { payer_id: req.query.PayerID }
+  paypal.payment.execute(paymentId, payerId, function(error, payment) {
     if (error) {
-      console.error(JSON.stringify(error));
+      console.error(JSON.stringify(error))
       return next(error)
     } else {
       if (payment.state == 'approved') {
-        console.log('payment completed successfully');
-        console.log(payment);
+        console.log('payment completed successfully')
+        console.log(payment)
         res.json({ payment })
       } else {
-        console.log('payment not successful');
+        console.log('payment not successful')
       }
     }
   })
@@ -289,4 +296,4 @@ function categorizeQueryString(queryObj) {
   return { query, order }
 }
 
-module.exports = router;
+module.exports = router
--- routes/users.js
+++ routes/users.js
@@ -1,36 +1,38 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 const jwt = require('jsonwebtoken')
 const config = require('../configs/jwt-config')
 const ensureAuthenticated = require('../modules/ensureAuthenticated')
-const User = require('../models/User');
-const Cart = require('../models/Cart');
+const User = require('../models/User')
+const Cart = require('../models/Cart')
 const CartClass = require('../modules/Cart')
 const Product = require('../models/Product')
 const Variant = require('../models/Variant')
 const TypedError = require('../modules/ErrorHandler')
 
-
 //POST /signin
-router.post('/signin', function (req, res, next) {
+router.post('/signin', function(req, res, next) {
   const { fullname, email, password, verifyPassword } = req.body
-  req.checkBody('fullname', 'fullname is required').notEmpty();
-  req.checkBody('email', 'Email is required').notEmpty();
-  req.checkBody('password', 'Password is required').notEmpty();
-  req.checkBody('verifyPassword', 'verifyPassword is required').notEmpty();
-  let missingFieldErrors = req.validationErrors();
+  req.checkBody('fullname', 'fullname is required').notEmpty()
+  req.checkBody('email', 'Email is required').notEmpty()
+  req.checkBody('password', 'Password is required').notEmpty()
+  req.checkBody('verifyPassword', 'verifyPassword is required').notEmpty()
+  let missingFieldErrors = req.validationErrors()
+  debugger
   if (missingFieldErrors) {
     let err = new TypedError('signin error', 400, 'missing_field', {
-      errors: missingFieldErrors,
+      errors: missingFieldErrors
     })
     return next(err)
   }
-  req.checkBody('email', 'Email is not valid').isEmail();
-  req.checkBody('password', 'Passwords have to match').equals(req.body.verifyPassword);
+  req.checkBody('email', 'Email is not valid').isEmail()
+  req
+    .checkBody('password', 'Passwords have to match')
+    .equals(req.body.verifyPassword)
   let invalidFieldErrors = req.validationErrors()
   if (invalidFieldErrors) {
     let err = new TypedError('signin error', 400, 'invalid_field', {
-      errors: invalidFieldErrors,
+      errors: invalidFieldErrors
     })
     return next(err)
   }
@@ -38,43 +40,45 @@ router.post('/signin', function (req, res, next) {
     fullname: fullname,
     password: password,
     email: email
-  });
-  User.getUserByEmail(email, function (error, user) {
+  })
+  User.getUserByEmail(email, function(error, user) {
     if (error) return next(err)
     if (user) {
       let err = new TypedError('signin error', 409, 'invalid_field', {
-        message: "user is existed"
+        message: 'user is existed'
       })
       return next(err)
     }
-    User.createUser(newUser, function (err, user) {
-      if (err) return next(err);
+    User.createUser(newUser, function(err, user) {
+      if (err) return next(err)
       res.json({ message: 'user created' })
-    });
+    })
   })
-});
+})
 
 //POST /login
-router.post('/login', function (req, res, next) {
+router.post('/login', function(req, res, next) {
   const { email, password } = req.body.credential || {}
   if (!email || !password) {
-    let err = new TypedError('login error', 400, 'missing_field', { message: "missing username or password" })
+    let err = new TypedError('login error', 400, 'missing_field', {
+      message: 'missing username or password'
+    })
     return next(err)
   }
-  User.getUserByEmail(email, function (err, user) {
+  User.getUserByEmail(email, function(err, user) {
     if (err) return next(err)
     if (!user) {
-      let err = new TypedError('login error', 403, 'invalid_field', { message: "Incorrect email or password" })
+      let err = new TypedError('login error', 403, 'invalid_field', {
+        message: 'Incorrect email or password'
+      })
       return next(err)
     }
-    User.comparePassword(password, user.password, function (err, isMatch) {
+    User.comparePassword(password, user.password, function(err, isMatch) {
       if (err) return next(err)
       if (isMatch) {
-        let token = jwt.sign(
-          { email: email },
-          config.secret,
-          { expiresIn: '7d' }
-        )
+        let token = jwt.sign({ email: email }, config.secret, {
+          expiresIn: '7d'
+        })
         res.status(201).json({
           user_token: {
             user_id: user.id,
@@ -84,7 +88,9 @@ router.post('/login', function (req, res, next) {
           }
         })
       } else {
-        let err = new TypedError('login error', 403, 'invalid_field', { message: "Incorrect email or password" })
+        let err = new TypedError('login error', 403, 'invalid_field', {
+          message: 'Incorrect email or password'
+        })
         return next(err)
       }
     })
@@ -92,12 +98,14 @@ router.post('/login', function (req, res, next) {
 })
 
 //GET cart
-router.get('/:userId/cart', ensureAuthenticated, function (req, res, next) {
+router.get('/:userId/cart', ensureAuthenticated, function(req, res, next) {
   let userId = req.params.userId
-  Cart.getCartByUserId(userId, function (err, cart) {
+  Cart.getCartByUserId(userId, function(err, cart) {
     if (err) return next(err)
     if (cart.length < 1) {
-      let err = new TypedError('cart error', 404, 'not_found', { message: "create a cart first" })
+      let err = new TypedError('cart error', 404, 'not_found', {
+        message: 'create a cart first'
+      })
       return next(err)
     }
     return res.json({ cart: cart[0] })
@@ -105,76 +113,73 @@ router.get('/:userId/cart', ensureAuthenticated, function (req, res, next) {
 })
 
 //POST cart
-router.post('/:userId/cart', ensureAuthenticated, function (req, res, next) {
+router.post('/:userId/cart', ensureAuthenticated, function(req, res, next) {
   let userId = req.params.userId
   let { productId, increase, decrease } = req.body
   // let { color, size } = requestProduct.product
 
-  Cart.getCartByUserId(userId, function (err, c) {
+  Cart.getCartByUserId(userId, function(err, c) {
     if (err) return next(err)
     let oldCart = new CartClass(c[0] || { userId })
     // no cart save empty cart to database then return response
     if (c.length < 1 && !productId) {
-      return Cart.createCart(oldCart.generateModel(), function (err, resultCart) {
+      return Cart.createCart(oldCart.generateModel(), function(
+        err,
+        resultCart
+      ) {
         if (err) return next(err)
         return res.status(201).json({ cart: resultCart })
       })
     }
-    Product.findById(productId, function (e, product) {
+    Product.findById(productId, function(e, product) {
       if (e) {
-        e.status = 406;
-        return next(e);
+        e.status = 406
+        return next(e)
       }
       if (product) {
         if (decrease) {
-          oldCart.decreaseQty(product.id);
+          oldCart.decreaseQty(product.id)
         } else if (increase) {
-          oldCart.increaseQty(product.id);
+          oldCart.increaseQty(product.id)
         } else {
-          oldCart.add(product, product.id);
+          oldCart.add(product, product.id)
         }
         let newCart = oldCart.generateModel()
-        Cart.updateCartByUserId(
-          userId,
-          newCart,
-          function (err, result) {
-            if (err) return next(err)
-            return res.status(200).json({ cart: result })
-          })
+        Cart.updateCartByUserId(userId, newCart, function(err, result) {
+          if (err) return next(err)
+          return res.status(200).json({ cart: result })
+        })
       } else {
         // apply variant
-        Variant.getVariantByID(productId, function (e, variant) {
+        Variant.getVariantByID(productId, function(e, variant) {
           if (e) {
-            e.status = 406;
-            return next(e);
+            e.status = 406
+            return next(e)
           }
           if (variant) {
-            Product.getProductByID(variant.productID, function (e, p) {
-              let color = (variant.color) ? "- " + variant.color : "";
-              let size = (variant.size) ? "- " + variant.size : "";
-              variant.title = p.title + " " + color + size
+            Product.getProductByID(variant.productID, function(e, p) {
+              let color = variant.color ? '- ' + variant.color : ''
+              let size = variant.size ? '- ' + variant.size : ''
+              variant.title = p.title + ' ' + color + size
               variant.price = p.price
               if (decrease) {
-                oldCart.decreaseQty(variant.id);
+                oldCart.decreaseQty(variant.id)
               } else if (increase) {
-                oldCart.increaseQty(variant.id);
+                oldCart.increaseQty(variant.id)
               } else {
-                oldCart.add(variant, variant.id);
+                oldCart.add(variant, variant.id)
               }
               let newCart = oldCart.generateModel()
-              Cart.updateCartByUserId(
-                userId,
-                newCart,
-                function (err, result) {
-                  if (err) return next(err)
-                  res.status(200).json({ cart: result })
-                })
+              Cart.updateCartByUserId(userId, newCart, function(err, result) {
+                if (err) return next(err)
+                res.status(200).json({ cart: result })
+              })
             })
           }
           // no product and no variant find
           else {
             let err = new TypedError('/cart', 400, 'invalid_field', {
-              message: "invalid request body"
+              message: 'invalid request body'
             })
             return next(err)
           }
@@ -185,15 +190,15 @@ router.post('/:userId/cart', ensureAuthenticated, function (req, res, next) {
 })
 
 //PUT cart
-router.put('/:userId/cart', ensureAuthenticated, function (req, res, next) {
+router.put('/:userId/cart', ensureAuthenticated, function(req, res, next) {
   let userId = req.params.userId
   let requestProduct = req.body
   let { productId, color, size } = requestProduct.product
 
-  Cart.getCartByUserId(userId, function (err, c) {
+  Cart.getCartByUserId(userId, function(err, c) {
     if (err) return next(err)
     let oldCart = new CartClass(c[0] || {})
-    Product.getProductByID(productId, function (err, p) {
+    Product.getProductByID(productId, function(err, p) {
       if (err) return next(err)
       // console.log(`oldCart: \n${JSON.stringify(oldCart)}\n`);
       let newCart = oldCart.add(p, productId, { color, size })
@@ -209,10 +214,11 @@ router.put('/:userId/cart', ensureAuthenticated, function (req, res, next) {
             totalPrice: newCart.totalPrice,
             userId: userId
           },
-          function (err, result) {
+          function(err, result) {
             if (err) return next(err)
             res.json(result)
-          })
+          }
+        )
       } else {
         //no cart in database
         newCart = new Cart({
@@ -221,7 +227,7 @@ router.put('/:userId/cart', ensureAuthenticated, function (req, res, next) {
           totalPrice: newCart.totalPrice,
           userId: userId
         })
-        Cart.createCart(newCart, function (err, resultCart) {
+        Cart.createCart(newCart, function(err, resultCart) {
           if (err) return next(err)
           res.status(201).json(resultCart)
         })
@@ -230,4 +236,4 @@ router.put('/:userId/cart', ensureAuthenticated, function (req, res, next) {
   })
 })
 
-module.exports = router;
+module.exports = router
